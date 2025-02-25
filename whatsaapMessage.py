b6ae25b7b186e708dfe7ddae9e270f774304216c
Marcela-Gomez <marcelasaraigomezcanas@gmail.com>
2025-02-25 14:42:27 -0600

    feat:structure for templates

--- whatsaapMessage.py
+++ whatsaapMessage.py
@@ -15,26 +15,50 @@ headers = {
 }
 
 data = {
-        "messaging_product": "whatsapp",
-        "to": recipient,
-        "type": "template",
-        "template": { 
-                    "name" :"saludo",
-                    "language": { "code": "es" },
+    "messaging_product": "whatsapp",
+    "to": recipient,
+    "type": "template",
+    "template": { 
+                "name" :"additional_info",
+                "language": { "code": "es" },
                 "components": [
-                {
-                    "type": "header",
+                    {
+                    "type": "body",
                     "parameters": [
-                        {
-                            "type": "image",
-                            "image": { "link": "https://res.cloudinary.com/dnc8k8wwv/image/upload/v1735869010/destinations-235942_1280_sqdtc7.jpg" }
-                        }
-                    ]
-                }
-            ]   
-            }
-        } 
+                            {
+                                "type":"text",
+                                "parameter_name":"info",
+                                "text":" 🏠 Las ventanas de la cocina exterior están abiertas y puedes usarlas para acceder si es necesario. 🔒 Antes de finalizar tu estancia, asegúrate de cerrar todas las puertas y ventanas, apagar todas las luces interiores y exteriores, y dejar las llaves dentro de la casa."
+                            },
+                            {
+                                "type":"text",
+                                "parameter_name":"aparcamiento",
+                                "text":"No hay garaje privado en el edificio, pero justo enfrente encontrarás un parking privado con un precio aproximado de 23 € por día."
+                            },
+                            {
+                                "type":"text",
+                                "parameter_name":"asistencia",
+                                "text":"Si necesitas cualquier asistencia durante tu estancia o tienes alguna pregunta, no dudes en escribirnos. 📩 ¡Estamos aquí para ayudarte y asegurarnos de que tengas una experiencia inolvidable!",
+                            },
+                            {
+                                "type":"text",
+                                "parameter_name":"despedida",
+                                "text":"¡Esperamos que disfrutes al máximo de tu estancia en Avenida de Montecarlo 10, 2E, Benidorm - Urbanización Ciudad de las Antenas y que te lleves grandes recuerdos de esta experiencia! 🏔️✨",
+                            }
+                    ],}
+        ]   
+    }
+}
+""" 
+data = {
+    "messaging_product": "whatsapp",
+    "to": recipient,
+    "type": "text",
+    "text": {
+        "body": "Hola, soy un bot de prueba ¿En qué puedo ayudarte?"
+    }
 
-
+} """
 response = requests.post(whatsaap_url, json=data, headers=headers)
 print(response.json()) 
+
